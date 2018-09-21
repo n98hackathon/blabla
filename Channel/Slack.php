b953d1ee0e3ea012e5240539785aa0214ddd3962
Manuel Schmid <m.schmid@netz98.de>
2018-09-21 22:15:26 +0200

    Implement slack message sending

--- Channel/Slack.php
+++ Channel/Slack.php
@@ -2,12 +2,44 @@
 
 namespace N98Hackathon\BlaBla\Service;
 
+use GuzzleHttp\Client;
+use N98Hackathon\BlaBla\Api\ConfigInterface;
+
 class Slack
 {
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var ConfigInterface
+     */
+    protected $config;
+
+    /**
+     * Slack constructor.
+     *
+     * @param Client          $client
+     * @param ConfigInterface $config
+     */
+    public function __construct(Client $client, ConfigInterface $config)
+    {
+
+        $this->client = $client;
+        $this->config = $config;
+    }
+
     /**
      * @param string $message
      */
     public function send(string $message) {
-        // todo implement logic here
+        $slackWebhookUrl = $this->config->getSlackWebhookUrl();
+        $data = ['text' => $message];
+
+        $encodedMessage = json_encode($data);
+        $response = $this->client->post($slackWebhookUrl, [
+            'body' => $encodedMessage
+        ]);
     }
 }
