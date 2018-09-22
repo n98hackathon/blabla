11fe67e38bbf22a54b77bc8fb3466212f1162f69
Dmytro Cheshun <mitry@atwix.com>
2018-09-22 11:23:06 +0300

    Implementation of Search Results Notification plugin

--- Plugin/SearchNoResultsNotificationPlugin.php
+++ Plugin/SearchNoResultsNotificationPlugin.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace N98Hackathon\BlaBla\Observer;
+namespace N98Hackathon\BlaBla\Plugin;
 
-use Magento\Catalog\Model\Layer;
+use Magento\Catalog\Model\Layer\Search;
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\CatalogSearch\Helper\Data as CatalogSearchData;
 use N98Hackathon\BlaBla\Api\ChannelPoolInterface;
 
 /**
@@ -17,29 +18,43 @@ class SearchNoResultsNotificationPlugin
     protected $channelPool;
 
     /**
+     * @var CatalogSearchData
+     */
+    protected $catalogSearchData;
+
+    /**
      * SearchNoResultsNotificationPlugin constructor.
      *
      * @param ChannelPoolInterface $channelPool
+     * @param CatalogSearchData $catalogSearchData
      */
-    public function __construct(ChannelPoolInterface $channelPool)
-    {
+    public function __construct(
+        ChannelPoolInterface $channelPool,
+        CatalogSearchData $catalogSearchData
+    ) {
         $this->channelPool = $channelPool;
+        $this->catalogSearchData = $catalogSearchData;
     }
 
     /**
      * Submit notification when catalog search returns no results
      *
-     * @param Layer      $subject
+     * @param Search     $subject
      * @param Collection $productCollection
      *
      * @return Collection
      */
-    public function afterGetProductCollection(Layer $subject, Collection $productCollection)
+    public function afterGetProductCollection(Search $subject, Collection $productCollection)
     {
         $itemsCount = $productCollection->count();
 
         if (!$itemsCount) {
-            // TODO: Add implementation
+            $queryText = $this->catalogSearchData->getEscapedQueryText();
+            $channels = $this->channelPool->getChannels();
+
+            foreach ($channels as $channel) {
+                $channel->send(sprintf('The search returned no results. The query was: "%s".', $queryText));
+            }
         }
 
         return $productCollection;
