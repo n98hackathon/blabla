6db95ade7d97610a746c0f4a8e2ba791f4b4d423
Dmytro Cheshun <mitry@atwix.com>
2018-09-22 12:21:33 +0300

    Fix the config paths

--- Api/ConfigInterface.php
+++ Api/ConfigInterface.php
@@ -12,27 +12,27 @@ interface ConfigInterface
     /**
      * Enable / disable slack push notifications
      */
-    const XPATH_SLACK_ENABLED = 'n98hackathon/slack/is_enabled';
+    const XPATH_SLACK_ENABLED = 'n98hackathon_blabla/slack/is_enabled';
 
     /**
      * Webhook for sending slack messages to
      */
-    const XPATH_SLACK_WEBHOOK_URL = 'n98hackathon/slack/webhook_url';
+    const XPATH_SLACK_WEBHOOK_URL = 'n98hackathon_blabla/slack/webhook_url';
 
     /**
      * Enable / disable order purchase event push notifications
      */
-    const XPATH_ORDER_PURCHASE_ENABLED = 'n98hackathon/events/order_purchase_enabled';
+    const XPATH_ORDER_PURCHASE_ENABLED = 'n98hackathon_blabla/events/order_purchase_enabled';
 
     /**
      * Enable / disable customer creation event push notifications
      */
-    const XPATH_CUSTOMER_CREATION_ENABLED = 'n98hackathon/events/customer_creation_enabled';
+    const XPATH_CUSTOMER_CREATION_ENABLED = 'n98hackathon_blabla/events/customer_creation_enabled';
 
     /**
      * Enable / disable zero search results event push notifications
      */
-    const XPATH_ZERO_SEARCH_RESULTS_ENABLED = 'n98hackathon/events/zero_search_results_enabled';
+    const XPATH_ZERO_SEARCH_RESULTS_ENABLED = 'n98hackathon_blabla/events/zero_search_results_enabled';
 
     /**
      * @return bool
